23c6b7a74ab49996cc1fdd9e0c53e3cf8d0acbbc
AlexandrTsy <114476920+AlexandrTsy@users.noreply.github.com>
2022-09-27 12:14:10 +0300

    Update module5_homework.js

--- module5_homework.js
+++ module5_homework.js
@@ -72,3 +72,37 @@ console.log(cars.length);["ЗАЗ", "ИЖ", "АЗЛК", "ВАЗ", "ГАЗ"].forE
  };
 
 console.log([foo(arr)]);
+//res/ true
+
+//
+
+var arr = [5, 5, 4, 5];
+
+ function foo(a) {
+for (var b = 0; b < a.length; b++)
+if (a[b] !== a[0]) return !1;
+return !0
+ };
+
+console.log([foo(arr)]);
+//res/ false
+
+Задание 7
+let arr = [4, 3, 2, null, 1, 5, 99, 6,undefined, ];
+let zerocount = 0;
+let numbercount = 0;
+let evencount = 0;
+let oddcount = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number" || !isNaN(arr[i])) {
+        numbercount += 1;
+        if (arr[i] === 0) {
+            zerocount += 1;
+        } else if (arr[i] % 2 === 0) {
+            evencount += 1;
+        } else {
+            oddcount += 1;
+        }
+    }
+}
+console.log(`В массиве: ${numbercount} цифр, ${zerocount} нулей, ${evencount} чётных, ${oddcount} нечётных!`);
